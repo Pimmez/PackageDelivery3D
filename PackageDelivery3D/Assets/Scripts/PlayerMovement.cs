cd360a9295b3193b078844579e4829e2353fa0de
pimhoreman@gmail.com <pimhoreman@gmail.com>
2019-11-26 21:41:21 +0100

    -Added Houterige Movement

--- PackageDelivery3D/Assets/Scripts/PlayerMovement.cs
+++ PackageDelivery3D/Assets/Scripts/PlayerMovement.cs
@@ -2,17 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour, IInput
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[SerializeField] private float movementSpeed = 4f;
+	[SerializeField] private Rigidbody rb;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private void Awake()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	/// <summary>
+	/// FixedUpdate for Rigidbody and physics related actions.
+	/// </summary>
+	private void FixedUpdate()
+	{
+		Movement();
+	}
+
+	/// <summary>
+	/// This method is public because it derives from IInput (Interface).
+	/// </summary>
+	public void Movement()
+	{
+		float _horizontal = Input.GetAxisRaw("Horizontal");
+		float _vertical = Input.GetAxisRaw("Vertical");
+
+		Vector3 _tempVector = new Vector3(_horizontal, 0, _vertical);
+		_tempVector = _tempVector.normalized * movementSpeed * Time.deltaTime;
+		rb.MovePosition(transform.position + _tempVector);
+	}
 }
