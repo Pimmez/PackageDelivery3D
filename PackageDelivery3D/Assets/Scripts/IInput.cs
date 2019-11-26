d43bb6c6ee697e59768107fff70519011a509487
pimhoreman@gmail.com <pimhoreman@gmail.com>
2019-11-26 15:08:25 +0100

    -Added IInput.cs Interface. -Added Blue material with folder -Added Prefab Cube with folder -Added Scripts folder -Updated PimScene

---
+++ PackageDelivery3D/Assets/Scripts/IInput.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Dit is een basis interface met simpelen methodes.
+public interface IInput
+{
+	void Movement();
+}
+
+/// <summary>
+/// Dit is een generic interface where T is de placeholder voor een data type dat in de geimplimenteerde class wordt toegevoegd.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public interface IDamageable<T>
+{
+	void Damage(T _damageTaken);
+}
