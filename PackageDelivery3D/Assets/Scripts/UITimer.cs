61b3cde46f0b9dabf6c5bf75a8885065555804a4
pimhoreman@gmail.com <pimhoreman@gmail.com>
2019-11-26 21:17:45 +0100

    -Added UITimer.cs -Added new prefab player -Added Empty PlayerMovement.cs script -Added TextMeshPro -Updated PimScene

---
+++ PackageDelivery3D/Assets/Scripts/UITimer.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using TMPro; 
+
+public class UITimer : MonoBehaviour
+{
+	[Tooltip("Timer in Seconds")]
+	[SerializeField] private float timerSeconds = 60f;
+
+	[SerializeField] private TextMeshProUGUI timerText;
+
+	private void Awake()
+	{
+		timerText = GetComponentInChildren<TextMeshProUGUI>();
+	}
+
+	private void Update()
+	{
+		CountDown();
+	}
+
+	/// <summary>
+	/// Counts down the timer with Time.deltaTime.
+	/// </summary>
+	private void CountDown()
+	{
+		timerSeconds -= Time.deltaTime;
+		if (timerSeconds > 0)
+		{
+			UpdateLevelTimer(timerSeconds);
+		}
+		else
+		{
+			Debug.Log("Timer over");
+		}
+	}
+
+	/// <summary>
+	/// Small Calculation for the timer to be "00:00" when counting down.
+	/// </summary>
+	/// <param name="timerCounter"></param>
+	private void UpdateLevelTimer(float timerCounter)
+	{
+		int minutes = Mathf.FloorToInt(timerCounter / 60f);
+		int seconds = Mathf.RoundToInt(timerCounter % 60f);
+		string formatedSeconds = seconds.ToString();
+
+		if (seconds == 60)
+		{
+			seconds = 0;
+			minutes += 1;
+		}
+
+		timerText.text = minutes.ToString("00") + ":" + seconds.ToString("00");
+	}
+}
