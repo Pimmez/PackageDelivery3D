62453cf75d12eef139d81eeb3367e60bf07810b0
JHunter465 <47977920+JHunter465@users.noreply.github.com>
2019-11-27 09:07:21 +0100

    -Broke the Movement -Added Character Controls
    Omdat ik inheritance heb gebruikt voor de playermotor neemt hij nu natuurlijk ook alle serialized fields over en dat was ik even vergeten, volgende push stop ik met inheriten en geef ik enkel de waardes mee die nodig zijn of maak het zo dat je desnoods in 2 classes de waardes voor player movement moet geven

---
+++ PackageDelivery3D/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    //Declaration of Variables
+    [Header("Classes")]
+
+    [Tooltip("Script voor de PlayerMotor")]
+    [SerializeField] private PlayerMotor motor;
+
+    [Space]
+
+    [Header("Componenten")]
+
+    [Tooltip("Character Controller Component")]
+
+    [SerializeField] protected CharacterController controller;
+
+    [Space]
+
+    [Header("Movement Values")]
+
+    [Tooltip("Value voor voorwaartse Beweging")]
+    [SerializeField] private float forwardSpeed;
+
+    [Tooltip("Value voor zijwaartse Beweging")]
+    [SerializeField] private float sidewaysSpeed;
+
+    [Tooltip("Value voor SprongHoogte")]
+    [SerializeField] protected float jumpHeight;
+
+    [Tooltip("Value voor Zwaartekracht")]
+    [SerializeField] protected float gravity;
+
+    [Space]
+
+    [Header("Collision en Drop Values")]
+
+    [Tooltip("De transform van het object wat het laagste punt van de speler aangeeft")]
+    [SerializeField] protected Transform groundCheck;
+
+    [Tooltip("De radius van de cirkel die om de groundCheck wordt gecast")]
+    [SerializeField] protected float groundDistance = 0.4f;
+
+    [Tooltip("Layermask om te bepalen wat wordt gezien als de Ground")]
+    [SerializeField] protected LayerMask groundMask;
+
+    protected float forwardMovement;
+    protected float sidewaysMovement;
+
+    protected bool jump = false;
+    protected bool crouch = false;
+
+    private void Update()
+    {
+    /// <summary>
+    /// In deze reeks worden de Values van forwardMovement en sidewaysMovement aangepast als de Input veranderd
+    /// </summary>
+        if (Input.GetAxisRaw("Vertical") != 0)
+        {
+            forwardMovement = Input.GetAxisRaw("Vertical") * forwardSpeed * Time.deltaTime;
+        }
+        if (Input.GetAxisRaw("Horizontal") != 0)
+        {
+            sidewaysMovement = Input.GetAxisRaw("Horizontal") * sidewaysSpeed * Time.deltaTime;
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        motor.Move(forwardMovement, sidewaysMovement, jump, crouch);
+        jump = false;
+    }
+}
---
+++ PackageDelivery3D/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerMotor : PlayerController
+{
+    private Vector3 move;
+    private Vector3 velocity;
+    private bool isGrounded;
+
+    public void Move(float _forwardMovement, float _sidewaysMovement, bool jump, bool crouch)
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        move = sidewaysMovement * Vector3.right + forwardMovement * Vector3.forward;
+
+        controller.Move(move);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+    }
+}
