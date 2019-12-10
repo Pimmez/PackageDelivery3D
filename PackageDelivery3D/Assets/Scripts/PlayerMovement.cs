88c6ffb1d44f0e3a1427c44274376b2c61463c0d
pimhoreman@gmail.com <pimhoreman@gmail.com>
2019-12-10 12:05:58 +0100

    -Added TextureConveyor.mat -Added ScrollingTextures.cs -Updated Grey.mat

--- PackageDelivery3D/Assets/Scripts/PlayerMovement.cs
+++ PackageDelivery3D/Assets/Scripts/PlayerMovement.cs
@@ -10,6 +10,14 @@ public class PlayerMovement : MonoBehaviour, IInput
 		rb = GetComponent<Rigidbody>();
 	}
 
+	private void LookRotation()
+	{
+		if (rb.velocity != Vector3.zero)
+		{
+			transform.rotation = Quaternion.LookRotation(rb.velocity, Vector3.up);
+		}
+	}
+
 	public void Movement()
 	{
 		Debug.Log("DefaultStandalone Movement");
@@ -20,6 +28,8 @@ public class PlayerMovement : MonoBehaviour, IInput
 		Vector3 _movement = new Vector3(_moveHorizontal, 0.0f, _moveVertical);
 
 		rb.AddForce(_movement * speed, ForceMode.VelocityChange);
+		LookRotation();
+
 	}
 
 	public void WebMovement()
@@ -32,19 +42,20 @@ public class PlayerMovement : MonoBehaviour, IInput
 		Vector3 _movement = new Vector3(_moveVertical, 0.0f, _moveHorizontal);
 
 		rb.AddForce(_movement * speed, ForceMode.VelocityChange);
+		LookRotation();
 	}
 
 	//DO XBOX
 	public void ControllerMovement()
 	{
 		Debug.Log("Controller Movement");
-
+		LookRotation();
 	}
 
 	//DO ANDROID
 	public void MobileMovement()
 	{
 		Debug.Log("Mobile Movement");
-
+		LookRotation();
 	}
 }
